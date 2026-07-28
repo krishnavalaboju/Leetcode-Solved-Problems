1f29caf315811619353001bb495dbf353601d753
krishnavalaboju <krishnaavalaboju@gmail.com>
2026-07-28 20:08:53 +0530

    Time: 1 ms (97.42%), Space: 42.3 MB (90.64%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2)return x;
+        int l=1;
+        int r=x;
+        int a=0;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(m<=x/m){
+                a=m;
+                l=m+1;
+            }else{
+                r=m-1;
+            }
+        }
+       return a; 
+    }
+}
