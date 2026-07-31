057864ccb0087993a62f2d6d1a881ff3c1f3f21b
krishnavalaboju <krishnaavalaboju@gmail.com>
2026-07-31 10:11:10 +0530

    Time: 393 ms (59.81%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,5 +1,1 @@
-select e.name as Employee from Employee as e where exists(
-    select 1 from Employee m
-    where e.managerId=m.Id 
-    and e.salary >m.salary
-);
+select e.name as Employee from Employee as e join Employee as m on e.managerId=m.Id and e.salary>m.salary;
